d50512737560bcc0f891b233786a42f86f171aef
Sun-Mac <Sun@SunMac.local>
2016-12-05 11:10:18 +0800

    Initial commit

---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by Sun on 16/11/11.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class ApplicationTest {
+
+	@Test
+	public void exampleTest() {
+	}
+
+}
